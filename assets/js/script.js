6c46d79302745158494aba556142116a28a26f4b
waleedtarbosh <waleedtarbush@gmail.com>
2023-02-14 11:02:22 +0200

    create navbar and home

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,4 +8,40 @@ function loader(){
   
   window.onload = fadeOut();
 
-  
+
+  let menu = document.querySelector('#menu-bar');
+  let navbar = document.querySelector('.navbar');
+
+
+  window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+  let videoBtn = document.querySelectorAll('.vid-btn');
+
+  videoBtn.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src;
+    });
+});
